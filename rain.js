d8bebb355d9f881df4aa7f62fc64fd7f1b61e4d7
TejeshCodes <tejeshreddy907@gmail.com>
2020-05-14 14:44:34 +0530

    commit

---
+++ rain.js
@@ -0,0 +1,15 @@
+class Rain{
+    constructor(x, y){
+        'restitution':0.8,
+        'friction':1.0,
+        'density':1.0
+    }
+}
+
+display(){
+    for (var i = random(0, 9), i < 10, i = random(0, 9))
+    {
+        var a = random(0, 9);
+        
+    }
+}
